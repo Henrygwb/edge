a46878e0f42fb79dc4d847e7c3396f68a54e96a0
Wenbo Guo <wenboguo@wenbotekimbp.lan>
2021-05-29 15:31:04 -0400

    clean cartpole

--- cartpole/quantitative_eval.py
+++ cartpole/quantitative_eval.py
@@ -1,13 +1,10 @@
 import os, sys
 sys.path.append('..')
 os.environ["CUDA_VISIBLE_DEVICES"] = " "
-import gym
 import numpy as np
-# from utils import rl_fed
-from explainer.quantitative_test import truncate_importance, draw_fid_fig, draw_stab_fig, draw_fid_fig_t, compute_rl_fid
-# from stable_baselines import PPO2
-# from stable_baselines.common import make_vec_env
+from explainer.quantitative_test import truncate_importance, draw_fid_fig, draw_fid_fig_t, compute_rl_fid
 
+run_rl_fid = False
 
 encoder_type = 'MLP'
 rnn_cell_type = 'GRU'
@@ -48,226 +45,112 @@ rat_fid = rat_fid_results['fid']
 rat_stab = rat_fid_results['stab']
 
 # # Explainer 6 - DGP.
-dgp_1_fid_results = np.load(save_path + 'dgp/dgp_regression_GRU_100_False_False_False_False_False_False_True_1e-05_10_16_True_4_exp.npz')
+dgp_1_fid_results = np.load(
+    save_path + 'dgp_regression_GRU_100_False_False_False_False_False_False_True_0.01_10_16_True_4_exp.npz')
 dgp_1_sal = dgp_1_fid_results['sal']
 dgp_1_fid = dgp_1_fid_results['fid']
 dgp_1_stab = dgp_1_fid_results['stab']
 
-dgp_2_fid_results = np.load(save_path + 'dgp/dgp_regression_GRU_100_False_False_False_False_False_False_True_0.01_10_16_True_4_exp.npz')
-dgp_2_sal = dgp_2_fid_results['sal']
-dgp_2_fid = dgp_2_fid_results['fid']
-dgp_2_stab = dgp_2_fid_results['stab']
-
 # Model Fid/Stab figures.
-#
-# fid_all = np.vstack((rudder_fid[None, ], saliency_fid[None, ], attn_fid[None, ], rat_fid[None, ], dgp_1_fid[None, ],
-#                      dgp_2_fid[None, ]))
-#
-# explainer_all = ['Rudder', 'Saliency', 'Attention', 'RatNet', 'Our', 'Our_x']
-# metrics_all = ['ZeroOne', 'Top5', 'Top15', 'Top25']
-# save_fig_path = save_path+'model_fid_bar.pdf'
-# draw_fid_fig(fid_all, explainer_all, metrics_all, save_fig_path, box_plot=False, log_scale=False)
-#
-# stab_all = np.vstack((rudder_stab[None, ...], saliency_stab[None, ...], attn_stab[None,  ...], rat_stab[None,  ...],
-#                       dgp_1_stab[None, ...], dgp_2_stab[None, ...]))
-# explainer_all = ['Rudder', 'Saliency', 'Attention', 'RatNet', 'Our', 'Our_x']
-# metrics_all = ['ZeroOne', 'Top5', 'Top15', 'Top25']
-# save_stab_path = save_path+'model_stab_bar.pdf'
-# draw_stab_fig(stab_all, explainer_all, save_stab_path, box_plot=False)
-#
-# rudder_fid = np.vstack((rudder_fid, rudder_stab[None, ...]))
-# saliency_fid = np.vstack((saliency_fid, saliency_stab[None, ...]))
-# attn_fid = np.vstack((attn_fid, attn_stab[None, ...]))
-# rat_fid = np.vstack((rat_fid, rat_stab[None, ...]))
-# dgp_1_fid = np.vstack((dgp_1_fid, dgp_1_stab[None, ...]))
-# dgp_2_fid = np.vstack((dgp_2_fid, dgp_2_stab[None, ...]))
 
-# fid_all = np.vstack((rudder_fid[None, ...], saliency_fid[None, ...], attn_fid[None,  ...], rat_fid[None,  ...],
-#                      dgp_2_fid[None, ...]))
-# explainer_all = ['Rudder', 'Saliency', 'Attention', 'RatNet', 'Our']
-# metrics_all = ['ZeroOne', 'Top5', 'Top15', 'Top25']
-# save_fig_path = save_path+'figures_x_l1/model_fid_bar.pdf'
-# draw_fid_fig(fid_all, explainer_all, metrics_all, save_fig_path, box_plot=False, log_scale=False)
-#
-# stab_all = np.vstack((rudder_stab[None, ...], saliency_stab[None, ...], attn_stab[None,  ...], rat_stab[None,  ...],
-#                       dgp_2_stab[None, ...]))
-# explainer_all = ['Rudder', 'Saliency', 'Attention', 'RatNet', 'Our']
-# metrics_all = ['ZeroOne', 'Top5', 'Top15', 'Top25']
-# save_stab_path = save_path+'figures_x_l1/model_stab_bar.pdf'
-# draw_stab_fig(stab_all, explainer_all, save_stab_path, box_plot=False)
-#
-# rudder_fid = np.vstack((rudder_fid, rudder_stab[None, ...]))
-# saliency_fid = np.vstack((saliency_fid, saliency_stab[None, ...]))
-# attn_fid = np.vstack((attn_fid, attn_stab[None, ...]))
-# rat_fid = np.vstack((rat_fid, rat_stab[None, ...]))
-# dgp_2_fid = np.vstack((dgp_2_fid, dgp_2_stab[None, ...]))
-#
-# fid_all = np.vstack((rudder_fid[None, ...], saliency_fid[None, ...], attn_fid[None,  ...], rat_fid[None,  ...],
-#                      dgp_2_fid[None, ...]))
-# explainer_all = ['Rudder', 'Saliency', 'Attention', 'RatNet', 'Our']
-# metrics_all = ['ZeroOne', 'Top5', 'Top15', 'Top25', 'Stability']
-# save_stab_path = save_path+'figures_x_l1/model_fid_stab_bar.pdf'
-# draw_fid_fig(fid_all, explainer_all, metrics_all, save_stab_path, box_plot=False, log_scale=False)
+rudder_fid = np.vstack((rudder_fid, rudder_stab[None, ...]))
+saliency_fid = np.vstack((saliency_fid, saliency_stab[None, ...]))
+attn_fid = np.vstack((attn_fid, attn_stab[None, ...]))
+rat_fid = np.vstack((rat_fid, rat_stab[None, ...]))
+dgp_1_fid = np.vstack((dgp_1_fid, dgp_1_stab[None, ...]))
+
+fid_all = np.vstack((rudder_fid[None, ...], saliency_fid[None, ...], attn_fid[None,  ...], rat_fid[None,  ...],
+                     dgp_1_fid[None, ...]))
+
+fid_all = fid_all[:, 1:, :]
+
+explainer_all = ['Rudder', 'Saliency', 'Attention', 'RatNet', 'EDGE']
+metrics_all = ['Top10', 'Top20', 'Top30', 'Stability']
+save_fig_path = save_path+'model_fid_stab.pdf'
+draw_fid_fig(fid_all, explainer_all, metrics_all, save_fig_path, box_plot=False, log_scale=False)
 
 # Fid RL.fig
-"""
-env_name = 'CartPole-v1'
-max_ep_len = 200
-agent_path = './agents/ppo2_cartpole.zip'
+if run_rl_fid:
+    import gym
+    from utils import rl_fed
+    from stable_baselines import PPO2
+    from stable_baselines.common import make_vec_env
 
-# load ppo2 model
-model = PPO2.load(agent_path)
-model = model.act_model
+    env_name = 'CartPole-v1'
+    max_ep_len = 200
+    agent_path = './agents/ppo2_cartpole.zip'
 
-num_trajs = 4200
-env = make_vec_env(env_name, n_envs=1)
+    # load ppo2 model
+    model = PPO2.load(agent_path)
+    model = model.act_model
 
+    num_trajs = 4200
+    env = make_vec_env(env_name, n_envs=1)
 
-# Baseline fidelity
-diff_all_10 = np.zeros((5, num_trajs))
-diff_all_20 = np.zeros((5, num_trajs))
-diff_all_30 = np.zeros((5, num_trajs))
+    diff_all_10 = np.zeros((6, num_trajs))
+    diff_all_20 = np.zeros((6, num_trajs))
+    diff_all_30 = np.zeros((6, num_trajs))
 
-importance_len_10 = np.zeros((5, num_trajs))
-importance_len_20 = np.zeros((5, num_trajs))
-importance_len_30 = np.zeros((5, num_trajs))
-finals_all = np.zeros(num_trajs)
-exps_all = [sal_value, rudder_sal, saliency_sal, attn_sal, rat_sal]
-for k in range(5):
-    print(k)
-    importance = exps_all[k]
-    for i in range(num_trajs):
-        print(i)
-        value = importance[i,0]
-        if np.sum(importance[i,:] == value) == importance.shape[1]:
-            importance_traj = np.arange(max_ep_len)
-            np.random.shuffle(importance_traj)
-        else:
-            importance_traj = np.argsort(importance[i,])[::-1]
-        importance_traj_10 = truncate_importance(importance_traj, 10)
-        importance_traj_20 = truncate_importance(importance_traj, 20)
-        importance_traj_30 = truncate_importance(importance_traj, 30)
-        original_traj = np.load('trajs_exp/CartPole-v1_traj_{}.npz'.format(i))
-        orin_reward = original_traj['final_rewards']
+    importance_len_10 = np.zeros((6, num_trajs))
+    importance_len_20 = np.zeros((6, num_trajs))
+    importance_len_30 = np.zeros((6, num_trajs))
+    finals_all = np.zeros(num_trajs)
+    exps_all = [sal_value, rudder_sal, saliency_sal, attn_sal, rat_sal]
+    for k in range(6):
+        print(k)
+        importance = exps_all[k]
+        for i in range(num_trajs):
+            print(i)
+            value = importance[i,0]
+            if np.sum(importance[i,:] == value) == importance.shape[1]:
+                importance_traj = np.arange(max_ep_len)
+                np.random.shuffle(importance_traj)
+            else:
+                importance_traj = np.argsort(importance[i,])[::-1]
+            importance_traj_10 = truncate_importance(importance_traj, 10)
+            importance_traj_20 = truncate_importance(importance_traj, 20)
+            importance_traj_30 = truncate_importance(importance_traj, 30)
+            original_traj = np.load('trajs_exp/CartPole-v1_traj_{}.npz'.format(i))
+            orin_reward = original_traj['final_rewards']
 
-        if k == 0:
-            finals_all[i] = orin_reward
-        orin_reward = int(orin_reward * (200 - 106) + 106)
-        seed = int(original_traj['seeds'])
-        # rl_fed(env=env, seed=seed, model=model, original_traj=original_traj, max_ep_len=max_ep_len, importance=None,
-        #        render=False, mask_act=False)
-        replay_reward_10 = rl_fed(env=env, seed=seed, model=model, 
-                                  original_traj=original_traj, max_ep_len=max_ep_len, importance=importance_traj_10, 
-                                  render=False, mask_act=True)
-        replay_reward_20 = rl_fed(env=env, seed=seed, model=model,
-                                  original_traj=original_traj, max_ep_len=max_ep_len, importance=importance_traj_20,
-                                  render=False, mask_act=True)
-        replay_reward_30 = rl_fed(env=env, seed=seed, model=model,
-                                  original_traj=original_traj, max_ep_len=max_ep_len, importance=importance_traj_30,
-                                  render=False, mask_act=True)
+            if k == 0:
+                finals_all[i] = orin_reward
+            orin_reward = int(orin_reward * (200 - 106) + 106)
+            seed = int(original_traj['seeds'])
+            # rl_fed(env=env, seed=seed, model=model, original_traj=original_traj, max_ep_len=max_ep_len, importance=None,
+            #        render=False, mask_act=False)
+            replay_reward_10 = rl_fed(env=env, seed=seed, model=model,
+                                      original_traj=original_traj, max_ep_len=max_ep_len, importance=importance_traj_10,
+                                      render=False, mask_act=True)
+            replay_reward_20 = rl_fed(env=env, seed=seed, model=model,
+                                      original_traj=original_traj, max_ep_len=max_ep_len, importance=importance_traj_20,
+                                      render=False, mask_act=True)
+            replay_reward_30 = rl_fed(env=env, seed=seed, model=model,
+                                      original_traj=original_traj, max_ep_len=max_ep_len, importance=importance_traj_30,
+                                      render=False, mask_act=True)
 
-        diff_all_10[k, i] = np.abs(orin_reward-replay_reward_10)
-        diff_all_20[k, i] = np.abs(orin_reward-replay_reward_20)
-        diff_all_30[k, i] = np.abs(orin_reward-replay_reward_30)
-        importance_len_10[k, i] = len(importance_traj_10)
-        importance_len_20[k, i] = len(importance_traj_20)
-        importance_len_30[k, i] = len(importance_traj_30)
+            diff_all_10[k, i] = np.abs(orin_reward-replay_reward_10)
+            diff_all_20[k, i] = np.abs(orin_reward-replay_reward_20)
+            diff_all_30[k, i] = np.abs(orin_reward-replay_reward_30)
+            importance_len_10[k, i] = len(importance_traj_10)
+            importance_len_20[k, i] = len(importance_traj_20)
+            importance_len_30[k, i] = len(importance_traj_30)
 
-np.savez('fid_baselines.npz', diff_10=diff_all_10, diff_30=diff_all_30, diff_20=diff_all_20,
-         len_10=importance_len_10, len_30=importance_len_30, len_20=importance_len_20, rewards=finals_all)
+    np.savez(save_path+'fid_rl.npz', diff_10=diff_all_10, diff_30=diff_all_30, diff_20=diff_all_20,
+             len_10=importance_len_10, len_30=importance_len_30, len_20=importance_len_20, rewards=finals_all)
 
-print(np.sum(diff_all_10, 1))
-print(np.sum(diff_all_20, 1))
-print(np.sum(diff_all_30, 1))
+diff_10 = np.load('exp_results/fid_rl.npz')['diff_10']
+diff_20 = np.load('exp_results/fid_rl.npz')['diff_20']
+diff_30 = np.load('exp_results/fid_rl.npz')['diff_30']
 
-# DGP fidelity
-diff_all_10 = np.zeros((2, num_trajs))
-diff_all_20 = np.zeros((2, num_trajs))
-diff_all_30 = np.zeros((2, num_trajs))
-
-importance_len_10 = np.zeros((2, num_trajs))
-importance_len_20 = np.zeros((2, num_trajs))
-importance_len_30 = np.zeros((2, num_trajs))
-finals_all = np.zeros(num_trajs)
-exps_all = [dgp_1_sal, dgp_2_sal]
-for k in range(2):
-    print(k)
-    importance = exps_all[k]
-    for i in range(num_trajs):
-        print(i)
-        value = importance[i,0]
-        if np.sum(importance[i,:] == value) == importance.shape[1]:
-            importance_traj = np.arange(max_ep_len)
-            np.random.shuffle(importance_traj)
-        else:
-            importance_traj = np.argsort(importance[i,])[::-1]
-        importance_traj_10 = truncate_importance(importance_traj, 10)
-        importance_traj_20 = truncate_importance(importance_traj, 20)
-        importance_traj_30 = truncate_importance(importance_traj, 30)
-        original_traj = np.load('trajs_exp/CartPole-v1_traj_{}.npz'.format(i))
-        orin_reward = original_traj['final_rewards']
-        print(orin_reward)
-        if k == 0:
-            finals_all[i] = orin_reward
-        seed = int(original_traj['seeds'])
-        orin_reward = int(orin_reward * (200 - 106) + 106)
-        # rl_fed(env=env, seed=seed, model=model, original_traj=original_traj, max_ep_len=max_ep_len, importance=None,
-        #                                   render=False, mask_act=False)
-        replay_reward_10 = rl_fed(env=env, seed=seed, model=model, 
-                                  original_traj=original_traj, max_ep_len=max_ep_len, importance=importance_traj_10,
-                                  render=False, mask_act=True)
-        replay_reward_20 = rl_fed(env=env, seed=seed, model=model,
-                                  original_traj=original_traj, max_ep_len=max_ep_len, importance=importance_traj_20,
-                                  render=False, mask_act=True)
-        replay_reward_30 = rl_fed(env=env, seed=seed, model=model,
-                                  original_traj=original_traj, max_ep_len=max_ep_len, importance=importance_traj_30,
-                                  render=False, mask_act=True)
-
-        diff_all_10[k, i] = np.abs(orin_reward-replay_reward_10)
-        diff_all_20[k, i] = np.abs(orin_reward-replay_reward_20)
-        diff_all_30[k, i] = np.abs(orin_reward-replay_reward_30)
-        importance_len_10[k, i] = len(importance_traj_10)
-        importance_len_20[k, i] = len(importance_traj_20)
-        importance_len_30[k, i] = len(importance_traj_30)
-
-np.savez('fid_dgp.npz', diff_10=diff_all_10, diff_30=diff_all_30, diff_20=diff_all_20,
-         len_10=importance_len_10, len_30=importance_len_30, len_20=importance_len_20, rewards=finals_all)
-
-print(np.sum(diff_all_10, 1))
-print(np.sum(diff_all_20, 1))
-print(np.sum(diff_all_30, 1))
-"""
-
-a1 = np.load('exp_results/fid_baselines.npz')['diff_10']
-b1 = np.load('exp_results/fid_dgp.npz')['diff_10']
-diff_10 = np.vstack((a1, b1))
-
-a2 = np.load('exp_results/fid_baselines.npz')['diff_20']
-b2 = np.load('exp_results/fid_dgp.npz')['diff_20']
-diff_20 = np.vstack((a2, b2))
-
-a3 = np.load('exp_results/fid_baselines.npz')['diff_30']
-b3 = np.load('exp_results/fid_dgp.npz')['diff_30']
-diff_30 = np.vstack((a3, b3))
-
-a1 = np.load('exp_results/fid_baselines.npz')['len_10']
-b1 = np.load('exp_results/fid_dgp.npz')['len_10']
-len_10 = np.vstack((a1, b1))
-
-a2 = np.load('exp_results/fid_baselines.npz')['len_20']
-b2 = np.load('exp_results/fid_dgp.npz')['len_20']
-len_20 = np.vstack((a2, b2))
-
-a3 = np.load('exp_results/fid_baselines.npz')['len_30']
-b3 = np.load('exp_results/fid_dgp.npz')['len_30']
-len_30 = np.vstack((a3, b3))
-
-# Reward diff and explanation len figures
+len_10 = np.load('exp_results/fid_rl.npz')['len_10']
+len_20 = np.load('exp_results/fid_rl.npz')['len_20']
+len_30 = np.load('exp_results/fid_rl.npz')['len_30']
 
 eps = 0.001
-rl_fid_10 = compute_rl_fid(diff_10, len_10, diff_max=192.0, eps=eps, weight=2)
-rl_fid_20 = compute_rl_fid(diff_20, len_20, diff_max=192.0, eps=eps, weight=2)
-rl_fid_30 = compute_rl_fid(diff_30, len_30, diff_max=192.0, eps=eps, weight=2)
+rl_fid_10 = compute_rl_fid(diff_10, len_10, diff_max=diff_10.max(), eps=eps)
+rl_fid_20 = compute_rl_fid(diff_20, len_20, diff_max=diff_20.max(), eps=eps)
+rl_fid_30 = compute_rl_fid(diff_30, len_30, diff_max=diff_30.max(), eps=eps)
 
 print(np.mean(rl_fid_10, 1))
 print(np.std(rl_fid_10, 1))
@@ -276,19 +159,12 @@ print(np.std(rl_fid_20, 1))
 print(np.mean(rl_fid_30, 1))
 print(np.std(rl_fid_30, 1))
 
+rl_fid_10 = np.vstack((rl_fid_10[1:, :], rl_fid_10[0, :]))
+rl_fid_20 = np.vstack((rl_fid_20[1:, :], rl_fid_20[0, :]))
+rl_fid_30 = np.vstack((rl_fid_30[1:, :], rl_fid_30[0, :]))
 
-explainer_all = ['Value', 'Rudder', 'Saliency', 'Attention', 'RatNet', 'Our_1', 'Our_2']
-metrics_all = ['Top5', 'Top15', 'Top25']
-rl_fid_all = np.vstack((rl_fid_10[None, ...], rl_fid_30[None, ...], rl_fid_50[None,  ...]))
-draw_fid_fig_t(rl_fid_all, explainer_all, metrics_all, save_path+'rl_fid_bar.pdf', box_plot=False, log_scale=False)
-
-rl_fid_10 = np.vstack((rl_fid_10[0:5], rl_fid_10[6:]))
-rl_fid_30 = np.vstack((rl_fid_30[0:5], rl_fid_30[6:]))
-rl_fid_50 = np.vstack((rl_fid_50[0:5], rl_fid_50[6:]))
-rl_fid_all = np.vstack((rl_fid_10[None, ...], rl_fid_30[None, ...], rl_fid_50[None,  ...]))
-
-explainer_all = ['Value', 'Rudder', 'Saliency', 'Attention', 'RatNet', 'Our']
-metrics_all = ['Top5', 'Top15', 'Top25']
-draw_fid_fig_t(rl_fid_all, explainer_all, metrics_all, save_path+'figures_x_l1/rl_fid_bar.pdf',
+explainer_all = ['Rudder', 'Saliency', 'Attention', 'RatNet', 'EDGE', 'Value']
+metrics_all = ['Top10', 'Top20', 'Top30']
+rl_fid_all = np.vstack((rl_fid_10[None, ...], rl_fid_20[None, ...], rl_fid_30[None,  ...]))
+draw_fid_fig_t(rl_fid_all, explainer_all, metrics_all, save_path+'rl_fid.pdf',
                box_plot=False, log_scale=False)
-
--- cartpole/test_exp.py
+++ cartpole/test_exp.py
@@ -1,6 +1,6 @@
 import os, sys
 sys.path.append('..')
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = " "
 import numpy as np
 import argparse
 from explainer.DGP_XRL import DGPXRL
@@ -39,7 +39,6 @@ save_path = 'models/'
 likelihood_type = 'regression'
 n_stab_samples = 10
 
-
 if args.explainer == 'value':
     # Explainer 1 - Value function.
     values = []
@@ -67,11 +66,6 @@ elif args.explainer == 'rudder':
                                                                                             traj_path,
                                                                                             likelihood_type,
                                                                                             n_stab_samples)
-      print('=============================================')
-      print('Mean fid of the zero-one normalization: {}'.format(np.mean(fid_all[0])))
-      print('Std fid of the zero-one normalization: {}'.format(np.std(fid_all[0])))
-      print('Mean abs pred diff of the zero-one normalization: {}'.format(np.mean(abs_all[0])))
-      print('Std abs pred diff of the zero-one normalization: {}'.format(np.std(abs_all[0])))
 
       print('=============================================')
       print('Mean fid of the top 10 normalization: {}'.format(np.mean(fid_all[1])))
@@ -203,13 +197,6 @@ elif args.explainer == 'attention':
           exp_idx, batch_size, traj_path, n_stab_samples)
 
       print('=============================================')
-      print('Mean fid of the zero-one normalization: {}'.format(np.mean(fid_all[0])))
-      print('Std fid of the zero-one normalization: {}'.format(np.std(fid_all[0])))
-      print('Acc fid of the zero-one normalization: {}'.format(acc_all[0]))
-      print('Mean abs pred diff of the zero-one normalization: {}'.format(np.mean(abs_diff_all[0])))
-      print('Std abs pred diff of the zero-one normalization: {}'.format(np.std(abs_diff_all[0])))
-
-      print('=============================================')
       print('Mean fid of the top 10 normalization: {}'.format(np.mean(fid_all[1])))
       print('Std fid of the top 10 normalization: {}'.format(np.std(fid_all[1])))
       print('Acc fid of the top 10 normalization: {}'.format(acc_all[1]))
@@ -297,14 +284,14 @@ elif args.explainer == 'dgp':
       grid_bound = [(-3, 3)] * hiddens[-1] * 2
       likelihood_sample_size = 16
 
-      model_1 = 'dgp_regression_GRU_100_False_False_False_False_False_False_True_1e-05_10_16_True_4_model.data'
+      model_1 = 'dgp_regression_GRU_100_False_False_False_False_False_False_True_0.01_10_16_True_4_model.data'
       dgp_explainer = DGPXRL(train_len=30123, seq_len=seq_len, len_diff=len_diff, input_dim=input_dim,
                              hiddens=hiddens, likelihood_type=likelihood_type, lr=0.01, optimizer_type='adam',
                              n_epoch=2, gamma=0.1, num_inducing_points=100, n_action=n_action, embed_dim=embed_dim,
                              grid_bounds=grid_bound, encoder_type=encoder_type, inducing_points=None,
                              mean_inducing_points=None, num_class=num_class, rnn_cell_type=rnn_cell_type,
                              using_ngd=False, using_ksi=False, using_ciq=False, using_sor=False,
-                             using_OrthogonallyDecouple=False, weight_x=True, lambda_1=0.00001)
+                             using_OrthogonallyDecouple=False, weight_x=True, lambda_1=0.01)
 
       dgp_explainer.load(save_path+model_1)
       dgp_explainer.test(exp_idx, batch_size, traj_path, likelihood_sample_size=likelihood_sample_size)
@@ -316,7 +303,8 @@ elif args.explainer == 'dgp':
                           time=mean_time, acc=acc_all, full_covar=covar_all[0], traj_cova=covar_all[1],
                           step_covar=covar_all[2], abs_diff=abs_diff_all)
 
-      dgp_1_fid_results = np.load(save_path + 'dgp_regression_GRU_100_False_False_False_False_False_False_True_1e-05_10_16_True_4_exp.npz')
+      dgp_1_fid_results = np.load(
+            save_path + 'dgp_regression_GRU_100_False_False_False_False_False_False_True_0.01_10_16_True_4_exp.npz')
       dgp_1_sal = dgp_1_fid_results['sal']
       dgp_1_fid = dgp_1_fid_results['fid']
       dgp_1_stab = dgp_1_fid_results['stab']
@@ -351,65 +339,3 @@ elif args.explainer == 'dgp':
 
       print('=============================================')
       print('Mean exp time: {}'.format(dgp_1_time))
-
-      model_2 = 'dgp_regression_GRU_100_False_False_False_False_False_False_True_0.01_10_16_True_4_model.data'
-      dgp_explainer = DGPXRL(train_len=30123, seq_len=seq_len, len_diff=len_diff, input_dim=input_dim,
-                             hiddens=hiddens, likelihood_type=likelihood_type, lr=0.01, optimizer_type='adam',
-                             n_epoch=2, gamma=0.1, num_inducing_points=100, n_action=n_action, embed_dim=embed_dim,
-                             grid_bounds=grid_bound, encoder_type=encoder_type, inducing_points=None,
-                             mean_inducing_points=None, num_class=num_class, rnn_cell_type=rnn_cell_type,
-                             using_ngd=False, using_ksi=False, using_ciq=False, using_sor=False,
-                             using_OrthogonallyDecouple=False, weight_x=True, lambda_1=0.01)
-
-      dgp_explainer.load(save_path+model_2)
-      dgp_explainer.test(exp_idx, batch_size, traj_path, likelihood_sample_size=likelihood_sample_size)
-      name = model_2[:-11]
-      sal_rationale_all, covar_all, fid_all, stab_all, acc_all, abs_diff_all, mean_time = dgp_explainer.exp_fid_stab(
-          exp_idx, batch_size, traj_path, logit=True, n_stab_samples=n_stab_samples)
-
-      np.savez_compressed(save_path + name + '_exp.npz', sal=sal_rationale_all, fid=fid_all, stab=stab_all,
-                          time=mean_time, acc=acc_all, full_covar=covar_all[0], traj_cova=covar_all[1],
-                          step_covar=covar_all[2], abs_diff=abs_diff_all)
-      
-      dgp_2_fid_results = np.load(save_path + 'dgp_regression_GRU_100_False_False_False_False_False_False_True_0.01_10_16_True_4_exp.npz')
-      dgp_2_sal = dgp_2_fid_results['sal']
-      dgp_2_fid = dgp_2_fid_results['fid']
-      dgp_2_stab = dgp_2_fid_results['stab']
-      dgp_2_diff = dgp_2_fid_results['abs_diff']
-      dgp_2_time = dgp_2_fid_results['time']
-      dgp_2_acc = dgp_2_fid_results['acc']
-
-      print('=============================================')
-      print('Mean fid of the zero-one normalization: {}'.format(np.mean(dgp_2_fid[0])))
-      print('Std fid of the zero-one normalization: {}'.format(np.std(dgp_2_fid[0])))
-      print('Acc fid of the zero-one normalization: {}'.format(dgp_2_acc[0]))
-      print('Mean abs pred diff of the zero-one normalization: {}'.format(np.mean(dgp_2_diff[0])))
-      print('Std abs pred diff of the zero-one normalization: {}'.format(np.std(dgp_2_diff[0])))
-
-      print('=============================================')
-      print('Mean fid of the top 10 normalization: {}'.format(np.mean(dgp_2_fid[1])))
-      print('Std fid of the top 10 normalization: {}'.format(np.std(dgp_2_fid[1])))
-      print('Acc fid of the top 10 normalization: {}'.format(dgp_2_acc[1]))
-      print('Mean abs pred diff of the top 10 normalization: {}'.format(np.mean(dgp_2_diff[1])))
-      print('Std abs pred diff of the top 10 normalization: {}'.format(np.std(dgp_2_diff[1])))
-
-      print('=============================================')
-      print('Mean fid of the top 20 normalization: {}'.format(np.mean(dgp_2_fid[2])))
-      print('Std fid of the top 20 normalization: {}'.format(np.std(dgp_2_fid[2])))
-      print('Acc fid of the top 20 normalization: {}'.format(dgp_2_acc[2]))
-      print('Mean abs pred diff of the top 20 normalization: {}'.format(np.mean(dgp_2_diff[2])))
-      print('Std abs pred diff of the top 20 normalization: {}'.format(np.std(dgp_2_diff[2])))
-
-      print('=============================================')
-      print('Mean fid of the top 30 normalization: {}'.format(np.mean(dgp_2_fid[3])))
-      print('Std fid of the top 30 normalization: {}'.format(np.std(dgp_2_fid[3])))
-      print('Acc fid of the top 30 normalization: {}'.format(dgp_2_acc[3]))
-      print('Mean abs pred diff of the top 30 normalization: {}'.format(np.mean(dgp_2_diff[3])))
-      print('Std abs pred diff of the top 30 normalization: {}'.format(np.std(dgp_2_diff[3])))
-
-      print('=============================================')
-      print('Mean stab: {}'.format(np.mean(dgp_2_stab)))
-      print('Std stab: {}'.format(np.std(dgp_2_stab)))
-
-      print('=============================================')
-      print('Mean exp time: {}'.format(dgp_2_time))
